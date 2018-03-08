69914ebc734b3844c7df97bfe4c25a5feb7c9368
Minghao Shan <minghao8shan@gmail.com>
2018-03-07 23:10:53 -0600

    Refactored and commented UDP Server

--- src/Client-Server/UDPServer.java
+++ src/Client-Server/UDPServer.java
@@ -4,22 +4,29 @@ import java.io.IOException;
 import java.net.*;
 import java.util.*;
 
+/**
+ * The UDP Client
+ * @author      Minghao Shan, ...........
+ * @version     03/07/2018
+ */
 public class UDPServer implements Runnable
 {
     private DatagramSocket socket = null;
     private static HashMap<InetAddress, Integer> upNodes = new HashMap<>();
 
-
-
-    private UDPServer()
-    {
-    }
+    /**
+     * Constructor
+     */
+    private UDPServer(){}
 
     private static HashMap<InetAddress, Integer> getIPList()
     {
         return upNodes;
     }
 
+    /**
+     * Executes the server
+     */
     public void run()
     {
         System.out.println("Listening...");
@@ -30,36 +37,21 @@ public class UDPServer implements Runnable
 
             while (true)
             {
-                DatagramPacket incomingPacket = new DatagramPacket(incomingData,
-                        incomingData.length);
-                try {
-                    socket.receive(incomingPacket);
-                    String message = new String(incomingPacket.getData());
-                    InetAddress IPAddress = incomingPacket.getAddress();
-                    int port = incomingPacket.getPort();
-                    System.out.println("Received message from client: " + message);
-                    System.out.println("Client IP: " + IPAddress.getHostAddress());
-                    System.out.println("Client port: " + port);
-
-                    upNodes.put(IPAddress, 0);
-
-                    System.out.println("Clients: " + upNodes.toString() + "\n");
-
-                    String reply = upNodes.toString();
-                    byte[] data = reply.getBytes();
-                    /**
-                     * Debug
-                     */
-                    System.out.println(new String(data) + "\n\n");
-
-                    DatagramPacket replyPacket = new DatagramPacket(data, data.length, IPAddress, port);
-                    socket.send(replyPacket);
-                }
-                catch (SocketTimeoutException e)
-                {
-                    System.out.println("SocketTimeoutException");
-                    socket.close();
-                }
+                //receive the packet from client
+                DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
+                socket.receive(incomingPacket);
+                String message = new String(incomingPacket.getData());
+                InetAddress IPAddress = incomingPacket.getAddress();
+                int port = incomingPacket.getPort();
+                System.out.println("Received message from client: " + message);
+                System.out.println("Client IP: " + IPAddress.getHostAddress());
+                System.out.println("Client port: " + port);
+                upNodes.put(IPAddress, 0);
+                System.out.println("Clients: " + upNodes.toString() + "\n");
+                String reply = upNodes.toString();
+                byte[] data = reply.getBytes();
+                DatagramPacket replyPacket = new DatagramPacket(data, data.length, IPAddress, port);
+                socket.send(replyPacket);
             }
         }
 
@@ -69,10 +61,10 @@ public class UDPServer implements Runnable
         }
     }
 
-    public static void main(String[] args)
-    {
-
-        UDPServer server = new UDPServer();
+    /**
+     * Removes disconnected client.
+     */
+    public static void removeClient() {
         HashMap<InetAddress, Integer> upNodes = UDPServer.getIPList();
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
@@ -82,20 +74,26 @@ public class UDPServer implements Runnable
                 {
                     HashMap.Entry entry = (HashMap.Entry)iterator.next();
                     InetAddress key = (InetAddress) entry.getKey();
-                    int val = (Integer) entry.getValue();
-                    val++;
-                    if (val >= 30)
+                    int value = (Integer) entry.getValue();
+                    value++;
+                    if (value >= 30)
                     {
                         System.out.println(key.toString() + " has timed out" + "\n");
                         iterator.remove();
                     }
                     else
                     {
-                        upNodes.put(key, val);
+                        upNodes.put(key, value);
                     }
                 }
             }
         }, 0, 1000);
+    }
+    public static void main(String[] args)
+    {
+
+        UDPServer server = new UDPServer();
         server.run();
+        removeClient();
     }
 }
